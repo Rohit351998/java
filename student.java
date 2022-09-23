39df634aac06f7b60266a273302dfc9061cc7c54
Rohit351998 <114155835+Rohit351998@users.noreply.github.com>
2022-09-23 07:26:51 +0000

    Create student.java

---
+++ student.java
@@ -0,0 +1,20 @@
+class Teacher
+{
+  public void teach()
+  {
+   system.out.println("Teaching Students");
+  }
+}
+class Student Extends Teacher
+{
+  public void listen()
+  {
+    system.out.println("Listen Teachere");
+  }
+}
+public static void main(String args[])
+{
+  Student r = new student();
+  r.teach;
+  r.listen;
+}
