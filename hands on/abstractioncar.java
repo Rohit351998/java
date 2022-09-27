1992f3c05851aa8d6ac9638ff23478873624b916
Rohit351998 <114155835+Rohit351998@users.noreply.github.com>
2022-09-27 08:18:59 +0000

    Create abstractioncar.java

---
+++ hands on/abstractioncar.java
@@ -0,0 +1,25 @@
+abstract class car 
+{// constructor 
+  public void car()
+  {
+    system.out.println("car is built");
+  }
+}// creating abstract class
+abstrat void drive ()
+  public void drive()
+{
+  system.out.println("car change");
+}
+}
+public class tesla extends car 
+{
+  void drive()
+  {
+  system.out.println("drive safely");
+  }
+  public static void main(sting[]args)
+  tesla T = new tesla();// methods callings
+  T.drive=();
+  t.gearchange();
+  }
+  }
